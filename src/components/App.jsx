5a3f82b4f1d1fd5af2743e005ac3262941b1acca
qasklp <freeee386@gmail.com>
2023-11-04 03:52:43 +0200

    second try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,63 @@ import { Footer } from "./Footer/Footer";
 
 import { PerfectMan } from "./MediaPathes/PerfectMan";
 import { title1 } from "./MediaPathes/PerfectMan";
-import mainVideo from "../images/video/mainVideo.mp4"
+import mainVideo from "../images/video/mainVideo.mp4";
+
+import { useEffect, useState } from 'react';
+import Hammer from 'hammerjs';
 
 export const App = () => {
+  const [startX, setStartX] = useState(null);
+
+  const handlePan = (event) => {
+    if (startX !== null) {
+      const carrousel = event.target.closest('ul[data-carrousel]');
+
+      if (carrousel) {
+        console.log(`${startX} w ${event.changedPointers[0].clientX}`)
+        let deltaX = event.changedPointers[0].clientX - startX;
+        console.log(`${deltaX} c ${carrousel.scrollLeft}`)
+
+        if ((deltaX > 0 && carrousel.scrollLeft === 0) || 
+          (deltaX < 0 && carrousel.scrollLeft + carrousel.offsetWidth >= carrousel.scrollWidth)) {
+          deltaX = 0; 
+      }
+
+      if (Math.abs(deltaX) >= 10) { 
+        carrousel.scrollLeft -= deltaX;
+      }
+      }
+    }
+  }
+
+  const handlePanStart = (event) => {
+    setStartX(event.center.x);
+  }
+
+  const handlePanEnd = () => {
+    setStartX(null);
+  }
+
+  useEffect(() => {
+    const elements = document.querySelectorAll('ul[data-carrousel]');
+    document.querySelectorAll('img').forEach(function(img) {
+      img.addEventListener('mousedown', function(event) {
+        event.preventDefault();
+      });
+    });
+
+    elements.forEach(element => {
+      const hammer = new Hammer(element);
+
+      hammer.on('pan', handlePan);
+      hammer.on('panstart', handlePanStart);
+      hammer.on('panend', handlePanEnd);
+
+      return () => {
+        hammer.destroy();
+      };
+    });
+  });
     
   return (<>
     <VideoSection src={mainVideo} first={true} />
--- src/components/PhotoSection/PhotoSection.jsx
+++ src/components/PhotoSection/PhotoSection.jsx
@@ -1,62 +1,12 @@
 import PropTypes from 'prop-types';
 import styles from "./PhotoSection.module.css";
-import { useState, useEffect } from 'react';
 
 import { nanoid } from 'nanoid';
 
 export const PhotoSection = ({ mediaList, title }) => {
 
-    const [startX, setStartX] = useState(null);
-    const [xDiff, setXDiff] = useState(0);
-    let [isMouseDown, setIsMouseDown] = useState(false);
-    let [bias, setBias] = useState(0);
-
-    const handleMouseUp = () => {
-        if (isMouseDown) {
-            console.log("up")
-            setIsMouseDown(false);
-            setXDiff(0);
-        }
-    }
-
-    useEffect(() => {
-        document.addEventListener('mouseup', handleMouseUp);
-
-        return () => {
-            document.removeEventListener('mouseup', handleMouseUp);
-        }
-    }, [isMouseDown]);
-
-
-
-    const handleMouseDown = (e) => {
-        setIsMouseDown(true);
-        setStartX(e.clientX);
-    }
-
-    const handleMouseMove = (e) => {
-
-        if (isMouseDown) {
-            setXDiff(e.clientX - startX);
-            console.log(xDiff);
-            if (xDiff > 5) {
-                console.log(`${-Math.abs(xDiff / 2)} qqq `)
-                if (bias <= -Math.abs(xDiff / 2)) {
-                    console.log(`${-Math.abs(xDiff / 2)} qqq `)
-                    setBias(bias + xDiff/2);
-                } else {
-                    setBias(0);
-                }
-                console.log(`left bias ${bias}`)
-            } else if (xDiff < - 5) {
-                setBias(bias + xDiff/2);
-                console.log(`right bias ${bias}`)
-            }
-        }
-    }
-
     return <section className={styles.carrousel}>
-        <ul className={styles.contentList} style={{ '--bias': `${bias}px` }} onMouseDown={handleMouseDown} onMouseMove={handleMouseMove}>
+        <ul className={styles.contentList} data-carrousel>
             {mediaList.map(item => {
                 if (item.type === 'photo') {
                 return <li className={styles.contentItem} key={nanoid()}><img className={styles.photo} src={item.src} alt="work example" /></li>;
